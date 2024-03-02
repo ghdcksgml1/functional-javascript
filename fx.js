fa10127c4575ed60fc68e84abef42ed045e9e2f6
ghdcksgml1 <ghdcksgml2@naver.com>
2024-03-02 18:25:40 +0900

    feat: section 5, 6

--- fx.js
+++ fx.js
@@ -1,20 +1,23 @@
-const map = (f, iter) => {
+const curry = f =>
+    (a, ..._) => _.length ? f(a, ..._) : (..._) => f(a, ..._);
+
+const map = curry((f, iter) => {
     let res = [];
     for (const a of iter) {
         res.push(f(a));
     }
     return res;
-};
+});
 
-const filter = (cond, iter) => {
+const filter = curry((cond, iter) => {
     const res = [];
     for (const a of iter) {
         if (cond(a)) res.push(a);
     }
     return res
-}
+});
 
-const reduce = (f, num, iter) => {
+const reduce = curry((f, num, iter) => {
     if (!iter) {
         iter = num[Symbol.iterator]();
         num = iter.next().value;
@@ -24,4 +27,9 @@ const reduce = (f, num, iter) => {
         num = f(num, a);
     }
     return num;
+});
+
+const go = (...args) => {
+    return reduce((a, f) => f(a), args);
 };
+const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
